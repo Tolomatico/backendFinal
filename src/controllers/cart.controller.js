363452e9695a5463a624b8217d8978207ef09589
Tolomatico <ballesty.t@gmail.com>
2024-04-16 15:13:55 -0300

    Se agrego stock

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -5,6 +5,8 @@ const ticketModel = require("../models/ticket.model")
 const { generateCode } = require("../utils/equations")
 const usersModel = require("../models/users.model.js")
 const cartManager = new CartManager()
+const ProductManager = require("../dao/db/product-manager-db.js")
+const productManager = new ProductManager()
 
 class CartController {
 
@@ -18,28 +20,39 @@ class CartController {
             const products = cart.products
             const user = await usersModel.findOne({ cart: cartId })
 
-            const amount=products.reduce((acc,item)=>{
+            const noStockProducts = []
+
+            for (const item of products) {
+                const id = item.product
+                const product = await productManager.getProductById(id)
+                if (product.stock < item.quantity) {
+                    noStockProducts.push(item)
+
+                } else {
+                    product.stock = product.stock - item.quantity
+                    product.save()
+                }
+            }
+
+
+            const amount = products.reduce((acc, item) => {
                 return acc + (item.product.price * item.quantity)
-            },0)
+            }, 0)
 
-              const ticket=new ticketModel({
-                  code:generateCode(),
-                  purchase_datetime:Date.now(),
-                  amount:amount,
-                 purchaser:user._id
-              })
 
-              await ticket.save()
+            const ticket = new ticketModel({
+                code: generateCode(),
+                purchase_datetime: Date.now(),
+                amount: amount,
+                purchaser: user._id
+            })
+
+            await ticket.save()
             response(res, 201, `Se ah generado el ticket correctamente`)
 
         } catch (error) {
             response(res, 404, `Error al generar el ticket: ${error}`)
         }
-
-
-
-
-
 
     }
 
